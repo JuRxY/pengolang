078d17964687a90ab310572cf470f6e218ec4c61
JuRe07 <85701889+JuRxY@users.noreply.github.com>
2023-02-05 19:35:17 +0100

    Add files via upload

---
+++ app.pan
@@ -0,0 +1,6 @@
+FOR i = 1 TO 6 THEN
+    PRINT(i)
+END
+
+PRINT("____________")
+PRINT("Exit Status:")
---
+++ shell.py
@@ -0,0 +1,14 @@
+import pangolang
+
+while True:
+	text = input('pangolang >>  ')
+	if text.strip() == "": continue
+	result, error = pangolang.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
